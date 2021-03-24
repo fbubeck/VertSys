d8c3290ee12293537cd392851c841069122b8f87
fbubeck <ferdinand.bubeck@gmx.de>
2021-03-24 11:42:31 +0100

    data model

--- backend/src/main/java/wwi19b4/bubeck/backend/model/Fahrrad.java
+++ backend/src/main/java/wwi19b4/bubeck/backend/model/Fahrrad.java
@@ -1,2 +1,35 @@
-package wwi19b4.bubeck.backend.model;public class Fahrrad {
+package wwi19b4.bubeck.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fahrrad implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Fahrradshop fahrradshop;
+
+    @Column(length = 100)
+    @NotNull(message = "Modell muss vergeben werden")
+    @Size(min=1, max=100)
+    private String modell = "";
+
+    private int größeZoll = 0;
+
+    @Column(length = 100)
+    private String typ = "";
+
 }
--- backend/src/main/java/wwi19b4/bubeck/backend/model/Hersteller.java
+++ backend/src/main/java/wwi19b4/bubeck/backend/model/Hersteller.java
@@ -1,2 +1,39 @@
-package wwi19b4.bubeck.backend.model;public class Hersteller {
+package wwi19b4.bubeck.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hersteller implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Fahrrad fahrrad;
+
+    @Column(length = 100)
+    @NotNull(message = "Name muss vergeben werden")
+    @Size(min=1, max=100)
+    private String name = "";
+
+    @Column(length = 100)
+    @NotNull(message = "Adresse muss vergeben werden")
+    @Size(min=1, max=100)
+    private String adresse = "";
+
+    @NotNull(message = "Telefonnummer muss vergeben werden")
+    private long telefon = 0;
+
+    private long anzahlMitarbeiter = 0;
 }
